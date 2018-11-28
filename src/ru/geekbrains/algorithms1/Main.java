cae16f93e62e7636bcffd5df1ac86658e806b37a
sinister524 <sinister524@gmail.com>
2018-11-28 23:48:44 +0300

    Домашнее задание 1

---
+++ src/ru/geekbrains/algorithms1/Main.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.algorithms1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Находим среднее арифмитическое O(8)
+                int [] numbers = {37,65,13,55,88,65,99,146};
+                int k = 0;
+                for (int i = 0; i <numbers.length ; i++) {
+                    k+=numbers [i];
+                }
+                int average;
+                average = k / numbers.length;
+                System.out.println("Среднее арифмитическое: " + average);
+
+
+    }
+}
